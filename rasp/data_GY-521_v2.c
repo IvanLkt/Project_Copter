084c09afb243353cabf99d3fd67709d811de9b06
Ivan Laktionov <wains777@gmail.com>
2017-04-22 16:04:43 +0300

    Update data_GY-521_v2.c

--- rasp/data_GY-521_v2.c
+++ rasp/data_GY-521_v2.c
@@ -28,7 +28,7 @@ int main (int argc, char *argv[])
         {
                 for (;;)
                 {
-                        uint8_t* data=wiringPiI2CRead(fd);
+                        data=wiringPiI2CRead(fd);
                         gyroX = wiringPiI2CReadReg16(fd, 0x43);
                         gyroY = wiringPiI2CReadReg16(fd, 0x45);
                         gyroZ = wiringPiI2CReadReg16(fd, 0x47);
@@ -41,9 +41,9 @@ int main (int argc, char *argv[])
                         else
                         {
                                 //print data
-                                printf("gyro_xout: ", gyroX, " scaled: ", (gyroX / 131));
-                                printf("gyro_yout: ", gyroY, " scaled: ", (gyroY / 131));
-                                printf("gyro_zout: ", gyroZ, " scaled: ", (gyroZ / 131));
+                                printf("gyro_xout:%d\n", gyroX);
+                                printf("gyro_yout:%d\n", gyroY);
+                                printf("gyro_zout:%d\n", gyroZ);
                         }
                 }
         }
