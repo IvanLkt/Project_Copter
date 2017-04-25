afea44311a27ecff12c740a153ec18d24fe3faaa
Ivan Laktionov <wains777@gmail.com>
2017-04-25 18:04:31 +0300

    Create save_data.c

---
+++ rasp/save_data.c
@@ -0,0 +1,25 @@
+#include <time.h>
+#include <math.h>
+
+clock_t start, current_time;
+double speed;
+bool status_of_flight;
+
+struct point_3D {
+    int x;
+    int y;
+    int z;
+};
+
+typedef struct point_3D Point;
+
+
+
+int main()
+{
+    start = clock() / CLOCKS_PER_SEC;
+
+
+
+
+}
