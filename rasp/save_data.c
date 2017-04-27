6dd3b673d0888d11308a944330621d6702dbb45a
Ivan Laktionov <wains777@gmail.com>
2017-04-27 23:33:07 +0300

    Update save_data.c

--- rasp/save_data.c
+++ rasp/save_data.c
@@ -8,12 +8,30 @@ double speed;
 bool status_of_flight;
 
 
-struct point_3D {
+typedef struct {
     double x, y;
     int z;
-};
+    Point *next;
+    Point *prev;    
+} Point;
 
-typedef struct point_3D Point;
+typedef struct {
+    Point *start = NULL;
+    Point *finish = start;
+    int numrecords = 0;
+} Dynamic_array;
+
+void init()
+{
+    Dynamic_array *database = (Dynamic_array *);
+    database.start = NULL;
+    database.finish = database.start;
+}
+ 
+void add_point (Point *new_point)
+{
+    if !database.
+}
 
 void Enter_point(point* Array_of_point)
 {
