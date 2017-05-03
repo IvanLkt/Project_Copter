16ebbe0996d59fcc8ca76143ff5238e4e6d30a65
Ivan Laktionov <wains777@gmail.com>
2017-05-03 23:45:11 +0300

    Update time.c

--- rasp/time.c
+++ rasp/time.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <time.h>
+#include <wiringPi.h>
 
 int main(void)
 {
   time_t timer = time(NULL);
   printf("%d\n", timer);
-  clock_t timer2;
-  gettimeofday(CLOCK_MONOTONIC_RAW, &timer2);
-  uint64_t delta_us = timer2.tv_sec * 1000000 + timer2.tv_nsec / 1000;
+  unsigned long timer2;
+  timer2 = millis();
   printf("%d\n", delta_us);
   return 0;
 }
