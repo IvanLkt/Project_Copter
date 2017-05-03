8ee83b3b5fc159913acdb4d160045b046277f1b7
Ivan Laktionov <wains777@gmail.com>
2017-05-04 00:48:35 +0300

    Update time.c

--- rasp/time.c
+++ rasp/time.c
@@ -3,8 +3,11 @@
 
 int main () {
 clock_t tm;
-for (int i = 0; i<100000; i++){}
+for (int i = 0; i<1000; i++)
+{
+  printf("%d \n", i);
+}
 tm = clock();
 long k = tm*1000/CLOCKS_PER_SEC;
-printf("%ld /n", k);
+printf("%ld \n", k);
 }
