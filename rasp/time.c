3b6e25bf8f18e5d850e6b61e2526a2702397c87a
Ivan Laktionov <wains777@gmail.com>
2017-05-03 23:35:27 +0300

    Update time.c

--- rasp/time.c
+++ rasp/time.c
@@ -8,7 +8,7 @@ int main(void)
   clock_t timer2;
   gettimeofday(CLOCK_MONOTONIC_RAW, &timer2);
   uint64_t delta_us = timer2.tv_sec * 1000000 + timer2.tv_nsec / 1000;
-  printf("%d\n", delta_us;
+  printf("%d\n", delta_us);
   return 0;
 }
 
