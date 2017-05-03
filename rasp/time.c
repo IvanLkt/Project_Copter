6d24a1e0fab896a0d709a0bfa8d07b372c9765c8
Ivan Laktionov <wains777@gmail.com>
2017-05-04 00:40:43 +0300

    Update time.c

--- rasp/time.c
+++ rasp/time.c
@@ -1,20 +1,6 @@
-#include <sys/time.h>
-struct timeval tv1, tv2, dtv;
-struct timezone tz;
-//функция для начала замера времени, сохраняющая в переменной tv1 время начала измерений
-void time_start() 
-{ gettimeofday(&tv1, &tz); }
-//функция для окончания замера времени, возвращающая количество миллисекунд, прошедших с начала замера времени
-long time_stop()
-{ gettimeofday(&tv2, &tz);
-  dtv.tv_sec= tv2.tv_sec  - tv1.tv_sec;	//разница секунд
-  dtv.tv_usec=tv2.tv_usec - tv1.tv_usec;	//разница микросекунд
-  if (dtv.tv_usec<0) { dtv.tv_sec--; dtv.tv_usec+=1000000; }
-  //возвращение количества прошедших миллисекунд
-  return dtv.tv_sec*1000 + dtv.tv_usec/1000;  
-}
+#include <time.h>
 int main {
-   time_start();
-   delay(500);
-   long o = time_stop();
-   printf("%lf", o);
+clock_t tm;
+tm = clock()
+printf("%ld", tm*1000/CLOCKS_PER_SEC);
+}
