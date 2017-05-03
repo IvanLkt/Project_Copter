1a0282936b213640899fe3728ea25c48a1c9015e
Ivan Laktionov <wains777@gmail.com>
2017-05-04 00:14:23 +0300

    Update time.c

--- rasp/time.c
+++ rasp/time.c
@@ -1,15 +1,27 @@
-#include <stdio.h>
-#include <time.h>
-#include <wiringPi.h>
-#include <stdint.h>
+#include <stdio.h> //Для printf
+#include <time.h>  //Для clock_gettime
 
-int main(void)
-{
-  time_t timer = time(NULL);
-  printf("%d\n", timer);
-  uint32_t timer2;
-  timer2 = millis();
-  printf("%d\n", timer2);
-  return 0;
-}
+int main (void)
+{  
+   //Структуры для сохранения определенного времени
+   struct timespec mt1, mt2; 
+   //Переменная для расчета дельты времени
+   long int tt;      
+   
+   //Определяем текущее время
+   clock_gettime (CLOCK_REALTIME, &mt1);
+   
+   //Выводим определенное время на экран консоли
+   printf (“Секунды: %ld\n”,mt1.tv_sec);
+   printf (“Наносекунды: %ld\n”,mt1.tv_nsec);
 
+   //Определяем текущее время
+   clock_gettime (CLOCK_REALTIME, &mt2);
+
+   //Рассчитываем разницу времени между двумя измерениями
+   tt=1000000000*(mt2.tv_sec - mt1.tv_sec)+(mt2.tv_nsec - mt1.tv_nsec);
+
+   //Выводим результат расчета на экран
+   printf (“Затрачено время: %ld нс\n”,tt);
+
+   return 0;
