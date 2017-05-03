d6d38a74b69ca8a2017c532a3199f507b41ea2f6
Ivan Laktionov <wains777@gmail.com>
2017-05-03 22:51:51 +0300

    Update test.c

--- rasp/test.c
+++ rasp/test.c
@@ -1,23 +1,9 @@
-#include <stdio.h>   //Для printf
-#include <time.h>   //Для time, gmtime, asctime
+#include <stdio.h>
+#include <time.h>
 
-int main (void)
-{    
-   // Переменная для сохранения текущего системного времени
-   long int s_time;
-   // Указатель, в который будет помещен адрес структуры с 
-   // преобразованным временем
-   struct tm *m_time;
-
-   // Считываем текущее системное время
-   s_time = time (NULL);
-
-   // Преобразуем системное время в локальное
-   m_time  = gmtime (&s_time);
-
-   // С помощью функции asctime преобразуем локальное время в строку
-   // и выводим результат на консоль
-   printf (“Время: %f\n”, m_time);
-
-   return 0;
+int main(void)
+{
+  const time_t timer = time(NULL);
+  printf("%s\n", ctime(&timer));
+  return 0;
 }
