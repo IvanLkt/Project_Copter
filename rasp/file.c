f52282bfc35a4d0f5aad584008fa68d50747dcef
Ivan Laktionov <wains777@gmail.com>
2017-05-04 22:43:07 +0300

    add GPS function

--- rasp/file.c
+++ rasp/file.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <math.h>
+
+int line = 0;
 
 int main () {
-  typedef struct _Ground {
+  typedef struct Ground {
       double x,y;
   } Ground; //Coordinates of the control point
   int quantity_coordinates = 0;
@@ -17,5 +20,23 @@ int main () {
       //}
   printf("OK, %lf\n",Input_Coordinates[i].x);
   }
+
 return 0;
+}
+
+double speed (Groung *Input_Coordinates, U){
+  double x_1 = Input_Coordinates[0]->x; //широта
+  double y_1 = Input_Coordinates[0]->y; //долгота
+  double x_2 = Input_Coordinates[1]->x; //широта
+  double y_2 = Input_Coordinates[1]->y; //долгота
+  double k =0; //coefficient
+  k = (40074000/360)*sin(arctg(abs(y_2-y_1)/abs(x_2-x_1)))+(40074000/360)*cos(x_1)*cos(arctg(abs(y_2-y_1)/abs(x_2-x_1))); // (metr/derges)
+  return U/k;
+}
+
+void get_coordinate (Groung *Input_Coordinates, long *real_time, long *start_line_time, U){
+  double x = 0;
+  double y = 0;
+  x = Input_Coordinates[line+1]->x + (Input_Coordinates[line + 2]->x - Input_Coordinates[line]->x)*(*real_time - *start_line_time)*speed(Input_Coordinates, U)*(sqrt((Input_Coordinates[line+2]->x - Input_Coordinates[line+1]->x)^2 + (Input_Coordinates[line+2]->y - Input_Coordinates[line+1]->y)^2))^(-1);
+  y = Input_Coordinates[line+1]->y + (Input_Coordinates[line + 2]->y - Input_Coordinates[line]->y)*(*real_time - *start_line_time)*speed(Input_Coordinates, U)*(sqrt((Input_Coordinates[line+2]->x - Input_Coordinates[line+1]->x)^2 + (Input_Coordinates[line+2]->y - Input_Coordinates[line+1]->y)^2))^(-1);
 }
