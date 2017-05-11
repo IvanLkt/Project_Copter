dfaeff93ebb6274993dc850ac547e9e92bc4da95
Ivan Laktionov <wains777@gmail.com>
2017-05-11 12:31:52 +0300

    test

--- rasp/copter.c
+++ rasp/copter.c
@@ -322,6 +322,7 @@ int main (int argc, char *argv[]) {
             check_turn(database_angles);
             printf("POINT_3\n");
             if (line > 0) {
+                printf("POINT_4\n");
                 real_time_clocks = clock();
                 real_time = real_time_clocks * 1000 / CLOCKS_PER_SEC;
                 int alt = getCM();
@@ -329,14 +330,15 @@ int main (int argc, char *argv[]) {
                 double *Y;
                 get_coordinate(Input_Coordinates, real_time, start_line_time, U, X, Y);
                 add_point(database, *X, *Y, alt);
-                printf("POINT_4\n");
+                printf("POINT_5\n");
             }
             if (digitalRead(COPT) == LOW && start_time - real_time > 500) {
                 status_of_flight = false;
+                printf("POINT_6\n");
             }
         }
     }
-    printf("POINT_5\n");
+    printf("POINT_7\n");
     FILE *output_data;
     output_data = fopen ("output_data.txt", "w");
     Point *tmp = database->head;
