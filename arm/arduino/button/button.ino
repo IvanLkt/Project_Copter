fe71bb70e1ec64b5ffc9cfdf52b19692b6e4832e
TexnarOlesa <cfytr.ru@yandex.ru>
2017-05-18 00:21:03 +0300

    Add files via upload

---
+++ arm/arduino/button/button.ino
@@ -0,0 +1,25 @@
+int buttonPin = 2;
+boolean button = LOW;  
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+
+}
+
+void loop() {
+  button = dedbounce( button, buttonPin);
+  Serial.print(button );
+  Serial.print("\n" );
+}
+
+boolean dedbounce( boolean last, int Pin){ // проверяет состояние кнопки, избегая дребезга
+  boolean current = digitalRead(Pin);
+  if (last != current)
+     {
+      delay(10);
+      current = digitalRead(Pin);
+     }
+return current;
+}
+
---
+++ arm/arduino/mu_2/mu_2.ino
@@ -0,0 +1,31 @@
+#include <SPI.h>
+#include "RF24.h"
+
+const uint64_t pipe = 0xE8E8F0F0E1LL; // адрес канала передачи
+RF24 radio(9,10);
+int msg[3];
+
+void setup(){
+  Serial.begin(9600);
+  //============================================================Модуль NRF24
+  radio.begin();                      // Включение модуля
+  radio.setAutoAck(1);                // Установка режима подтверждения приема;
+  radio.setDataRate(RF24_250KBPS);    // Устанавливаем скорость
+  radio.setChannel(10);               // Устанавливаем канал
+  radio.openReadingPipe(1,pipe);      // Открываем 1 канал приема
+  radio.startListening();             // Начинаем слушать эфир
+  
+}
+
+void loop(){
+  if (radio.available()){
+    while (radio.available()){
+      radio.read(&msg, sizeof(msg));
+      Serial.print(msg[0]);Serial.print("\t");
+      Serial.print(msg[1]);Serial.print("\t");
+      Serial.print(msg[2]);Serial.print("\t");
+      Serial.print("\n");
+    }
+  }
+}
+
